7f36d4c812ab9db0637ed2f2074de30a0e84c907
Hino Banzon <hino@sandboxx.us>
2017-12-01 16:33:53 -0500

    updated computation example using DisposableObserver and subscribeWith

--- app/src/main/java/com/hino/rxjavaplayground/MainActivity.java
+++ app/src/main/java/com/hino/rxjavaplayground/MainActivity.java
@@ -1,19 +1,15 @@
 package com.hino.rxjavaplayground;
 
-import android.os.Handler;
-import android.os.Looper;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
@@ -23,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.myLabel) TextView textView;
 
     private Observable<Integer> observable;
-    private Disposable disposable;
     private DisposableObserver<Integer> disposableObserver;
     private int sum = 0;
 
@@ -45,59 +40,47 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.subscribeButton)
     public void onButtonClick() {
-        Toast.makeText(this, "Button does nothing...", Toast.LENGTH_SHORT);
-
-        // create a disposable subscriber
-        //new Thread(() -> createNewDisposableObserver()).start();
-
-        // create disposable
-        new Thread(() -> createNewDisposable()).start();
-    }
-
-    private void createNewDisposable() {
-        disposable = observable
-                .observeOn(AndroidSchedulers.mainThread()) // subscriber observes on the main thread
-                .subscribeOn(Schedulers.computation()) // observable is called outside the main thread
-                .subscribe(t -> {
-                    Timber.d("onNext(): %d", t);
-                    sum += t;
-                    textView.setText(String.format("%d", t));
-                });
+        new Thread(() -> createNewDisposableObserver()).start();
     }
 
     private void createNewDisposableObserver() {
-        disposableObserver = observable.subscribeWith(
-                new DisposableObserver<Integer>() {
-                    @Override
-                    public void onNext(Integer integer) {
-                        Timber.d(String.format("onNext(): %d", integer));
-                        sum += integer;
-                    }
+        disposableObserver = observable
+                .observeOn(AndroidSchedulers.mainThread()) // subscriber observes on the main thread
+                .subscribeOn(Schedulers.computation())     // observable is called outside the main thread
+                .subscribeWith(
+                    new DisposableObserver<Integer>() {
+                        @Override
+                        public void onNext(Integer integer) {
+                            Timber.d(String.format("onNext(): %d", integer));
+                            sum += integer;
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Timber.e(e.getMessage(), e);
-                    }
+                            if (sum % 10 == 0) {
+                                Timber.d("showing progress update for multiples of 10: %d", sum);
+                                textView.setText(String.format("%d...", sum));
+                            }
+                        }
 
-                    @Override
-                    public void onComplete() {
-                        Timber.i("Completed with sum: %d", sum);
-                        new Handler(Looper.getMainLooper()).post(() -> textView.setText(String.format("%d", sum)));
+                        @Override
+                        public void onError(Throwable e) {
+                            Timber.e(e.getMessage(), e);
+                        }
+
+                        @Override
+                        public void onComplete() {
+                            // onComplete() will NOT be called if you dispose
+                            Timber.i("Completed with sum: %d", sum);
+                            textView.setText(String.format("%d", sum));
+                        }
                     }
-                });
+                );
     }
 
     @OnClick(R.id.unsubscribeButton)
     public void onUnsubscribeClick() {
-        Timber.i("You have unsubscribed");
+        Timber.i("You have un-subscribed!!!");
         if (disposableObserver != null) {
             disposableObserver.dispose();
         }
-
-        if (disposable != null) {
-            disposable.dispose();
-        }
-
         textView.setText(String.format("%d", sum));
     }
 
